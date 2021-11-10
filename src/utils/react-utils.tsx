6a2a6f881a5e2762edfb78dd600c869f7a26576c
Chris Allen <chrisjallendeveloper@gmail.com>
2021-11-10 22:17:52 +0000

    created useMapEvent hook

--- src/utils/react-utils.tsx
+++ src/utils/react-utils.tsx
@@ -5,4 +5,30 @@ export const useOnce = (callback, ...args) => {
   }, []);
 };
 
-
+/**
+ * Tasty little map event hook
+ * @returns
+ */
+function useMapEvents<events>() {
+  type Callback = (name: string, meta?: any) => void;
+  type GroupCallback = (groupName: string, name: string, meta?: any) => void;
+  let groupCallbacks: Callback[] = [];
+  let callbacks: GroupCallback[] = [];
+  return [
+    // onEvent
+    (callback: Callback) => {
+      callbacks.push(callback);
+    },
+    // onEvents
+    (callback: GroupCallback) => {
+      groupCallbacks.push(callback);
+    },
+    // mapEvent
+    groupName => {
+      return (eventName: string, meta?: any) => {
+        callbacks.forEach(callback => callback(eventName, meta));
+        groupCallbacks.forEach(callback => callback(groupName, eventName, meta));
+      };
+    }
+  ] as const;
+}
